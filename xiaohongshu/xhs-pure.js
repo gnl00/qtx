c7a51e22360fcafa2a9ce7800f25e6ee58aa61a8
gnl0 <gnl0@qq.com>
2022-06-14 15:44:30 +0800

    [update]: remove app splash ads

--- xiaohongshu/xhs-pure.js
+++ xiaohongshu/xhs-pure.js
@@ -5,14 +5,23 @@ const notifiTitle = "xhs-pure";
 let body = JSON.parse($response.body);
 
 if(body.hasOwnProperty('data') && body.data) {
+  // app splash
+  if(url.indexOf('v2/system_service/splash_config') && body.data.hasOwnProperty('ads_groups')){
+    console.log('xhs-pure handle app splash')
+    body.data.ads_groups.length = 0
+  }
 
   // homefeed tabbar
   if(url.indexOf('v1/system_service/config') && body.data.hasOwnProperty('tabbar')) {
+    console.log('xhs-pure handle homefeed tabbar')
+
     body.data.tabbar.tabs = body.data.tabbar.tabs.filter(item => item.title !== '购买');
   }
 
   // homefeed categories
   if(url.indexOf('v6/homefeed/categories')) {
+    console.log('xhs-pure handle homefeed categories')
+
     if(body.data.hasOwnProperty('categories')) {
       body.data.categories = body.data.categories.map(item => {
         if(item.oid.indexOf('_v3') === -1) {
@@ -23,7 +32,7 @@ if(body.hasOwnProperty('data') && body.data) {
     }
 
     if(body.data.hasOwnProperty('rec_categories')) {
-      body.data.rec_categories = [];
+      delete body.data.rec_categories;
     }
   }
   
