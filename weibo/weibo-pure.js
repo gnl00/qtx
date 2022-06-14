47178a0404f89d110bd1cdd87b92bde9fc3354d6
gnl0 <gnl0@qq.com>
2022-06-14 23:07:49 +0800

    [update] weibo-pure

--- weibo/weibo-pure.js
+++ weibo/weibo-pure.js
@@ -4,10 +4,16 @@ const getMethod = "GET";
 const notifiTitle = "xhs-pure";
 let body = JSON.parse($response.body);
 
-if(body.hasOwnProperty('data')) {
-  if(url.indexOf('2/video/tiny_stream_video_list') !== -1) {
+// recommand video
+
+if(url.indexOf('2/video/tiny_stream_video_list') !== -1 && body.hasOwnProperty('data')) {
+  console.log('weibo-pure handle tiny_stream_video_list')
     body.data = {}
   }
+
+if(url.indexOf('2/!/multimedia/playback/batch_get') !== -1 && body.hasOwnProperty('list')) {
+  console.log('weibo-pure handle batch_get')
+  body.list = []
 }
 
 body = JSON.stringify(body);
