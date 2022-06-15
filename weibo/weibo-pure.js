e179e08b3b17bf984bbe295ec8a31fc875a2186f
gnl0 <gnl0@qq.com>
2022-06-15 13:06:29 +0800

    [update] weibo-pure remove more entertainment content

--- weibo/weibo-pure.js
+++ weibo/weibo-pure.js
@@ -16,10 +16,21 @@ if(url.indexOf('2/!/multimedia/playback/batch_get') !== -1 && body.hasOwnPropert
 
 // 热搜移除娱乐内容
 if(url.indexOf('/portal.php?a=search_topic') !== -1 && body.data.length !== 0) {
-  body.data = body.data.filter(item => ['剧集', '电影', '综艺'].indexOf(item.subject_label) === -1 )
+  console.log('weibo-pure handle /portal.php?a=search_topic')
+  body.data = body.data.filter(entertainmentContentFilter)
+}
+if(url.indexOf('/portal.php?ct=feed&a=trends') !== -1 && body.data) {
+  console.log('weibo-pure handle /portal.php?ct=feed&a=trends')
+  body.data.search_topic.cards = body.data.search_topic.cards.filter(entertainmentContentFilter)
+}
+if(url.indexOf('/portal.php?ct=feed&a=search_topic') !== -1 && body.data) {
+  console.log('weibo-pure handle /portal.php?ct=feed&a=search_topic')
+  body.data = body.data.filter(entertainmentContentFilter)
 }
 
 body = JSON.stringify(body);
 $done({
     body
 });
+
+const entertainmentContentFilter = item => ['剧集', '电影', '综艺'].indexOf(item.subject_label) === -1
