853df6259eae92b4914424328bc47a7a688b2a31
gnl0 <gnl0@qq.com>
2022-06-15 12:38:27 +0800

    [update] weibo-pure 移除热搜娱乐内容

--- weibo/weibo-pure.js
+++ weibo/weibo-pure.js
@@ -5,15 +5,18 @@ const notifiTitle = "xhs-pure";
 let body = JSON.parse($response.body);
 
 // recommand video
-
 if(url.indexOf('2/video/tiny_stream_video_list') !== -1 && body.hasOwnProperty('data')) {
   console.log('weibo-pure handle tiny_stream_video_list')
     body.data = {}
   }
-
 if(url.indexOf('2/!/multimedia/playback/batch_get') !== -1 && body.hasOwnProperty('list')) {
   console.log('weibo-pure handle batch_get')
   body.list = []
+}
+
+// 热搜移除娱乐内容
+if(url.indexOf('/portal.php?a=search_topic') !== -1 && body.data.length !== 0) {
+  body.data = body.data.filter(item => ['剧集', '电影', '综艺'].indexOf(item.subject_label) === -1 )
 }
 
 body = JSON.stringify(body);
