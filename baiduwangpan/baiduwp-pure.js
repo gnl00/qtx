cd3fc24907918e7ff27217ea5665bdee7be830ad
gnl0 <gnl0@qq.com>
2022-06-16 20:42:09 +0800

    [add] add supports for baiduwangpan

--- baiduwangpan/baiduwp-pure.js
+++ baiduwangpan/baiduwp-pure.js
@@ -6,7 +6,7 @@ let body = JSON.parse($response.body);
 
 // 去除首页营销占位
 if(url.indexOf('api/usercfg?') !== -1 && body) {
-  body.user_new_define_cards = body.user_new_define_cards.filter(item => card_area_name && ['首页笔记-卡片', '最近', '卡片管理-卡片'].indexOf(card_area_name) !== -1)
+  body.user_new_define_cards = body.user_new_define_cards.filter(item => item.card_area_name && ['首页笔记-卡片', '最近', '卡片管理-卡片'].indexOf(item.card_area_name) !== -1)
 }
 
 body = JSON.stringify(body);
