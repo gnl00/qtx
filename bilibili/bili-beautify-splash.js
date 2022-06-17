53d1d140e90cc4aa75fd5f848a97aedb9ba18442
gnl0 <gnl0@qq.com>
2022-06-17 21:52:16 +0800

    [update] update bili-beautify, try to handle more splash ads

--- bilibili/bili-beautify-splash.js
+++ bilibili/bili-beautify-splash.js
@@ -1,18 +1,58 @@
-// beautify splash/event
+// bili-beautify handle splash
 const url = $request.url;
 const method = $request.method;
-const getMethod = "GET";
 const notifiTitle = "bili-beautify";
-let body = $response.body;
+let rawBody = $response.body;
 
-if(url.indexOf('x/v2/splash/event/list2') && body) {
-  
-  // splash/event
-  console.log('bilibeautify handle splash/event')
-  body = null
-  
+// spalsh/list
+if(url.indexOf('x/v2/splash/list') !== -1 && rawBody) {
+  console.log('bili-beautify handle splash/list')
+  handleRawBody()
+}
+
+// splash/show
+if(url.indexOf('x/v2/splash/show') !== -1 && rawBody) {
+  console.log('bili-beautify handle splash/show')
+  handleRawBody()
+}
+
+// splash/event/list2
+if(url.indexOf('x/v2/splash/event/list2') !== -1 && rawBody) {
+  console.log('bili-beautify handle splash/event/list2')
+  handleRawBody()
+}
+
+function handleRawBody() {
+  if(rawBody && typeof(rawBody) === 'string') {
+    // check if it is json
+    try {
+      // json, convert json to js object
+      const body = JSON.parse(rawBody)
+      if(body && body.hasOwnProperty('data') && body.data.hasOwnProperty('show')) {
+        delete body.data.show
+      } else if(body && body.hasOwnProperty('data') && body.data.hasOwnProperty('list') && body.data.list.length !== 0) {
+        body.data.list.forEach(item => {
+          if(item.duration && item.duration !== 0) item.duration = 0
+          // 2050 年
+          if(item.begin_time) item.begin_time = 2524608000
+          if(item.end_time) item.end_time = 2524608000
+        })
+      }
+
+      // convert js object back to json
+      rawBody = JSON.stringify(body)
+
+    } catch(e) {
+      if(e instanceof SyntaxError) {
+        // not json
+        rawBody = null
+      }
+    }
+  } else {
+    rawBody = null
+  }
 }
 
 $done({
-    body
+  rawBody
 });
--- bilibili/bili-beautify.js
+++ bilibili/bili-beautify.js
@@ -4,100 +4,87 @@ const getMethod = "GET";
 const notifiTitle = "bili-beautify";
 let body = JSON.parse($response.body);
 
-if (!body.hasOwnProperty('data')) {
-    console.log(url);
-    console.log("body:" + $response.body);
-    $notification.post(notifiTitle, url, "data字段错误");
-} else {
-    if (url.indexOf("x/v2/splash") !== -1 && method === getMethod) {
-        console.log('bili-beautify 开屏页' + (url.indexOf("splash/show") !== -1 ? 'show' : 'list'));
-        if (body.data.hasOwnProperty('show')) {
-            // 有时候返回的数据没有show字段
-            delete body.data.show;
-        }
-    } else if(url.indexOf('x/v2/splash/list') !== -1) {
-        // 开屏广告预加载，不可强制删除 @Cuttlefish
-        console.log('bili-beautify handle x/v2/splash/list')
-        if(body.data.hasOwnProperty('list')) {
-          body.data.list.forEach(item => {
-            if(item.duration && item.duration !== 0) item.duration = 0
-            // 2050 年
-            if(item.begin_time) item.begin_time = 2524608000
-            if(item.end_time) item.end_time = 2524608000
-          })
-        }
-    } else if (url.indexOf("resource/show/tab/v2") !== -1 && method === getMethod) {
-        // 右上角top栏
-        if (body.data.hasOwnProperty('top')) {
-            console.log('bili-beautify 首页 top 修改');
-            body.data.top = body.data.top.filter(item => '消息' === item.name);
-            fixPos(body.data.top);
-        }
-        // 顶部tab栏
-        if (body.data.hasOwnProperty('tab'))  {
-            console.log('bili-beautify 首页 tab 修改');
-            body.data.tab = body.data.tab.filter(item => ['推荐', '热门', '追番'].indexOf(item.name) !== -1 );
-            fixPos(body.data.tab);
-        }
-        // 底部bottom栏
-        if (body.data.hasOwnProperty('bottom')) {
-            console.log('bili-beautify 首页 bottom 修改');
-            body.data.bottom = body.data.bottom.filter(item => ['首页', '动态', '我的'].indexOf(item.name) !== -1);
-            fixPos(body.data.bottom);
-        }
-    } else if(url.indexOf('x/v2/activity/inline') !== -1 && body.data) {
-        console.log('bili-beautify handle 顶部消息旁边的活动')
+if(body.hasOwnProperty('data')) {
+  // 首页精简
+  if (url.indexOf("resource/show/tab/v2") !== -1 && method === getMethod) {
+    console.log('bili-beautify handle 首页');
+
+    // 首页右上角 top 栏，去掉除消息之外的内容
+    if (body.data.hasOwnProperty('top')) {
+        console.log('bili-beautify handle 首页 top');
+        body.data.top = body.data.top.filter(item => '消息' === item.name);
+        fixPos(body.data.top);
+    }
+
+    // 首页 tab 栏
+    if (body.data.hasOwnProperty('tab'))  {
+        console.log('bili-beautify handle 首页 tab');
+        body.data.tab = body.data.tab.filter(item => ['推荐', '热门', '追番'].indexOf(item.name) !== -1 );
+        fixPos(body.data.tab);
+    }
+    // 首页 bottom 栏
+    if (body.data.hasOwnProperty('bottom')) {
+        console.log('bili-beautify handle 首页 bottom');
+        body.data.bottom = body.data.bottom.filter(item => ['首页', '动态', '我的'].indexOf(item.name) !== -1);
+        fixPos(body.data.bottom);
+    }
+  }
+  // 去掉首页右上角 top 栏旁边的活动
+  if(url.indexOf('x/v2/activity/inline') !== -1 && body.data) {
+    console.log('bili-beautify handle 首页 top 活动')
+    body.data = {}
+  }
+  if (url.indexOf("x/resource/top/activity") !== -1 && method === getMethod) {
+    console.log('bili-beautify handle 首页 top 活动')
+    if(body.data.hasOwnProperty('online')) {
         body.data = {}
-    } else if (url.indexOf("x/v2/feed/index") !== -1 && method === getMethod) {
-        console.log('bili-beautify 推荐页');
-        if (body.data.hasOwnProperty('items')) {
-            body.data.items = body.data.items.filter(i => {
-                if (i.hasOwnProperty('card_type') && i.hasOwnProperty('card_goto')) {
-                    const cardType = i.card_type;
-                    const cardGoto = i.card_goto;
-                    if (cardType === 'banner_v8' && cardGoto === 'banner') {
-                        if (!i.hasOwnProperty('banner_item')) {
-                            console.log("body:" + $response.body);
-                            $notification.post(notifiTitle, '推荐页', "banner_item错误");
-                        } else {
-                            for (const v of i.banner_item) {
-                                if (!v.hasOwnProperty('type')) {
-                                    console.log("body:" + $response.body);
-                                    $notification.post(notifiTitle, '推荐页', "type错误");
-                                } else {
-                                    if (v.type === 'ad') {
-                                        console.log('banner广告');
-                                        return false;
-                                    }
+    }
+  }
+
+  // 去广告
+  if (url.indexOf("x/v2/feed/index") !== -1 && method === getMethod) {
+    console.log('bili-beautify handle 推荐页广告');
+    if (body.data.hasOwnProperty('items')) {
+        body.data.items = body.data.items.filter(i => {
+            if (i.hasOwnProperty('card_type') && i.hasOwnProperty('card_goto')) {
+                const cardType = i.card_type;
+                const cardGoto = i.card_goto;
+                if (cardType === 'banner_v8' && cardGoto === 'banner') {
+                    if (!i.hasOwnProperty('banner_item')) {
+                        console.log("body:" + $response.body);
+                        $notification.post(notifiTitle, '推荐页', "banner_item错误");
+                    } else {
+                        for (const v of i.banner_item) {
+                            if (!v.hasOwnProperty('type')) {
+                                console.log("body:" + $response.body);
+                                $notification.post(notifiTitle, '推荐页', "type错误");
+                            } else {
+                                if (v.type === 'ad') {
+                                    console.log('banner广告');
+                                    return false;
                                 }
                             }
                         }
-                    } else if (cardType === 'cm_v2' && ['ad_web_s', 'ad_av', 'ad_web_gif', 'ad_player'].includes(cardGoto)) {
-                        // ad_player大视频广告 ad_web_gif大gif广告 ad_web_s普通小广告 ad_av创作推广广告
-                        console.log(`${cardGoto}广告去除)`);
-                        return false;
-                    } else if (cardType === 'small_cover_v10' && cardGoto === 'game') {
-                        console.log('游戏广告去除');
-                        return false;
-                    } else if (cardType === 'cm_double_v9' && cardGoto === 'ad_inline_av') {
-                        console.log('创作推广-大视频广告');
-                        return false;
                     }
-                } else {
-                    console.log("body:" + $response.body);
-                    $notification.post(notifiTitle, '推荐页', "无card_type/card_goto");
+                } else if (cardType === 'cm_v2' && ['ad_web_s', 'ad_av', 'ad_web_gif', 'ad_player'].includes(cardGoto)) {
+                    // ad_player大视频广告 ad_web_gif大gif广告 ad_web_s普通小广告 ad_av创作推广广告
+                    console.log(`${cardGoto}广告去除)`);
+                    return false;
+                } else if (cardType === 'small_cover_v10' && cardGoto === 'game') {
+                    console.log('游戏广告去除');
+                    return false;
+                } else if (cardType === 'cm_double_v9' && cardGoto === 'ad_inline_av') {
+                    console.log('创作推广-大视频广告');
+                    return false;
                 }
-                return true;
-            });
-        }
-    } else if (url.indexOf("x/resource/top/activity") !== -1 && method === getMethod) {
-        console.log('bili-beautify 顶部 top 活动')
-        if(body.data.hasOwnProperty('online')) {
-            body.data = {}
-        }
-    } else {
-        $notification.post(notifiTitle, "路径/请求方法匹配错误:", method + "," + url);
+            } else {
+                console.log("body:" + $response.body);
+                $notification.post(notifiTitle, '推荐页', "无card_type/card_goto");
+            }
+            return true;
+        });
     }
+  }
 }
 
 body = JSON.stringify(body);
