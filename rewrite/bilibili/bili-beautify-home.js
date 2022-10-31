bcab7cfaa6197741cb0401d3b9f15b1db76c1dfb
gnl <gnl0@qq.com>
2022-10-31 21:46:41 +0800

    [update] bili-beautify

--- rewrite/bilibili/bili-beautify-home.js
+++ rewrite/bilibili/bili-beautify-home.js
@@ -10,27 +10,27 @@ if(body && body.hasOwnProperty('data')) {
     console.log('bili-beautify handle 首页');
 
     // 首页 top 栏右侧，去掉除消息之外的内容
-    if (body.data.hasOwnProperty('top')) {
+    if (body.data && body.data.hasOwnProperty('top')) {
         console.log('bili-beautify handle 首页 top');
         body.data.top = body.data.top.filter(item => '消息' === item.name);
         fixPos(body.data.top);
     }
     
   // 首页 top 栏左侧，去掉头像左下角的 story
-  if (body.data.hasOwnProperty('top_left')) {
+  if (body.data && body.data.hasOwnProperty('top_left')) {
     console.log('bili-beautify handle 首页 story');
     delete body.data.top_left
   }
 
     // 首页 tab 栏
-    if (body.data.hasOwnProperty('tab'))  {
+    if (body.data && body.data.hasOwnProperty('tab'))  {
         console.log('bili-beautify handle 首页 tab');
         body.data.tab = body.data.tab.filter(item => ['推荐', '热门', '追番'].indexOf(item.name) !== -1 );
         fixPos(body.data.tab);
     }
     
     // 首页 bottom 栏
-    if (body.data.hasOwnProperty('bottom')) {
+    if (body.data && body.data.hasOwnProperty('bottom')) {
         console.log('bili-beautify handle 首页 bottom');
         body.data.bottom = body.data.bottom.filter(item => ['首页', '动态', '消息','我的'].indexOf(item.name) !== -1);
         fixPos(body.data.bottom);
@@ -45,7 +45,7 @@ if(body && body.hasOwnProperty('data')) {
   
   if (url.indexOf("x/resource/top/activity") !== -1 && method === getMethod) {
     console.log('bili-beautify handle 首页 top 活动')
-    if(body.data.hasOwnProperty('online')) {
+    if(body.data && body.data.hasOwnProperty('online')) {
         body.data = {}
     }
   }
--- rewrite/bilibili/bili-beautify-mine.js
+++ rewrite/bilibili/bili-beautify-mine.js
@@ -14,6 +14,14 @@ if(body.hasOwnProperty('data')) {
     body.data.sections_v2 = body.data.sections_v2.filter(item => ['创作中心', '推荐服务'].indexOf(item.title) === -1)
   }
 
+  // unlock 1080p 高码率 @ddgksf2013
+  if(url.indexOf("x/v2/account/myinfo") !== -1 && body.data && body.data.vip) {
+    if(body.data.vip.type) body.data.vip.type = 2
+    if(body.data.vip.status) body.data.vip.status = 1
+    if(body.data.vip.vip_pay_type) body.data.vip.vip_pay_type = 1
+    if(body.data.vip.due_date) body.data.vip.due_date = 4669824160
+  }
+
   // 去除青少模式每天两次提醒
   if(url.indexOf('x/v2/account/mode/status') !== -1 && body.data.length !== 0) {
     console.log('bili-beautify handle account/mode/status')
