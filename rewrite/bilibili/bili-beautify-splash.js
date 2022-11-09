ed6de2c214a4ac399c6e57f36f2dcee4553212d5
gnl <gnl0@qq.com>
2022-11-09 12:50:52 +0800

    [update]

--- rewrite/bilibili/bili-beautify-splash.js
+++ rewrite/bilibili/bili-beautify-splash.js
@@ -30,32 +30,28 @@ if(url.indexOf('x/v2/splash/event/list2') !== -1 && rawBody) {
 // }
 
 function handleRawBody() {
-  if(rawBody && typeof(rawBody) === 'string') {
+  if(rawBody) {
     // check if it is json
     try {
       // json, convert json to js object
       const body = JSON.parse(rawBody)
-      if(body && body.hasOwnProperty('data') && body.data.hasOwnProperty('show')) {
-        body.data = {}
-      } else if(body && body.hasOwnProperty('data') && body.data.hasOwnProperty('list') && body.data.list.length !== 0) {
-        // 有些情况下不能强制删除开屏广告 @ddgksf2013
-        body.data.list.forEach(item => {
-          if(item.duration && item.duration !== 0) item.duration = 0
-          // 2050 年
-          if(item.begin_time) item.begin_time = 2524608000
-          if(item.end_time) item.end_time = 2524608000
-        })
+      if(body && body.hasOwnProperty('data')) {
+        if(body.data.hasOwnProperty('show')) body.data = {}
+        if(body.data.hasOwnProperty('list') && body.data.list.length !== 0) {
+          body.data.list.forEach(item => {
+            if(item.duration) item.duration = 0
+            // 2050 年
+            if(item.begin_time) item.begin_time = 2524608000
+            if(item.end_time) item.end_time = 2524608000
+          })
+        }
       }
-
       // convert js object back to json
       rawBody = JSON.stringify(body)
-
     } catch(e) {
       // not json
       rawBody = null
     }
-  } else {
-    rawBody = null
   }
 }
 
