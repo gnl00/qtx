3e2c197231281ab1ae5a3328034a34a9b51259cf
gnl <gnl0@qq.com>
2022-11-10 14:26:55 +0800

    [update]

--- rewrite/gaodemap/gdmap-pure.js
+++ rewrite/gaodemap/gdmap-pure.js
@@ -1,14 +1,26 @@
 const url = $request.url;
 const method = $request.method;
 const notifiTitle = "gaodemap-pure";
-let response = JSON.parse($response.body);
+let response = $response.body;
+let body = null
+let isJSON = true
 
 // spalsh
-if(url.indexOf('ws/aos/alimama/splash_screen_rt') !== -1 && response.data) {
-  response.data = {}
+if(url.indexOf('ws/aos/alimama/splash_screen_rt') !== -1) {
+  console.log('gdmap-pure handle ws/aos/alimama/splash_screen_rt')
+  try {
+    body = JSON.parse(response);
+    if(body.data) body.data = {}
+  } catch (e) {
+    isJSON = false
+  }
 }
 
-response = JSON.stringify(response);
+let doneOBJ = null
+if(isJSON) {
+ doneOBJ = JSON.stringify(body)
+}
+
 $done({
-  response
+  body: doneOBJ
 });
